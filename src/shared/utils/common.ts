ee1c8e2ad796c7746278daeff9b5279da0362ef0
hyunseok.kim <dragmove@naver.com>
2023-03-29 19:29:46 +0900

    :lipstick: add UI components related to storybook test

--- src/shared/utils/common.ts
+++ src/shared/utils/common.ts
@@ -2,3 +2,9 @@ export function isDefined<T>(val: T): val is NonNullable<T> {
   if (val == null || typeof val === 'undefined') return false;
   return true;
 }
+
+export function curryr2(fn: (a: unknown, b: unknown) => any) {
+  return (b: unknown) => (a: unknown) => fn.apply(null, [a, b]);
+}
+
+export const eq = curryr2((a: unknown, b: unknown) => a === b);
