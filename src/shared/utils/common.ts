6504e37b39170eddf758e3792c18d1b3c3ff1291
hyunseok.kim <dragmove@naver.com>
2023-03-29 11:11:00 +0900

    :white_check_mark: restore failing unit-test

--- src/shared/utils/common.ts
+++ src/shared/utils/common.ts
@@ -1,4 +1,4 @@
 export function isDefined<T>(val: T): val is NonNullable<T> {
-  // if (val == null || typeof val === 'undefined') return false;
+  if (val == null || typeof val === 'undefined') return false;
   return true;
 }
